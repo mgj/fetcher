ac176bf8fcf4ebf56ae4e760712790205de0e2e2
Mikkel Jensen <mgj@artm.dk>
2017-05-06 04:02:59 +0200

    rc

--- source/Fetcher.Core/Services/IFetcherRepositoryStoragePathService.cs
+++ source/Fetcher.Core/Services/IFetcherRepositoryStoragePathService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace artm.Fetcher.Core.Services
+﻿namespace artm.Fetcher.Core.Services
 {
     public interface IFetcherRepositoryStoragePathService
     {
