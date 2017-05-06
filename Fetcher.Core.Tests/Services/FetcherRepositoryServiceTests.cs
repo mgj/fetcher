69b7e347d4cf490ddfc6774b270f44d419d6384b
Mikkel Jensen <mgj@artm.dk>
2017-05-06 20:42:27 +0200

    tests

--- Fetcher.Core.Tests/Services/FetcherRepositoryServiceTests.cs
+++ Fetcher.Core.Tests/Services/FetcherRepositoryServiceTests.cs
@@ -47,5 +47,51 @@ namespace artm.Fetcher.Core.Tests.Services
             var entry = sut.GetEntryForUrl(url);
             Assert.IsNotNull(entry);
         }
+
+        [Test]
+        public void UpdateUrl_UrlExists_UrlEntryIsUpdated()
+        {
+            var url = new Uri("https://www.google.com");
+            var response = "myTestResponse";
+            var sut = FetcherRepositoryService();
+
+            sut.InsertUrl(url, response);
+            var original = sut.GetEntryForUrl(url);
+            var originalResponse = original.Response;
+            sut.UpdateUrl(url, original, "UpdatedTestResponse");
+            var second = sut.GetEntryForUrl(url);
+
+            Assert.IsTrue(!originalResponse.Equals(second.Response));
+        }
+
+        [Test]
+        public void InsertUrl_ValidInput_UrlIsInserted()
+        {
+            var url = new Uri("https://www.google.com");
+            var response = "myTestResponse";
+            var sut = FetcherRepositoryService();
+
+            var isNull = sut.GetEntryForUrl(url);
+            sut.InsertUrl(url, response);
+            var notNull = sut.GetEntryForUrl(url);
+
+            Assert.IsNull(isNull);
+            Assert.NotNull(notNull);
+        }
+
+        [Test]
+        public void PreloadUrl_ValidInput_UrlIsPreloaded()
+        {
+            var url = new Uri("https://www.google.com");
+            var response = "myTestResponse";
+            var sut = FetcherRepositoryService();
+
+            var isNull = sut.GetEntryForUrl(url);
+            sut.PreloadUrl(url, response);
+            var notNull = sut.GetEntryForUrl(url);
+
+            Assert.IsNull(isNull);
+            Assert.NotNull(notNull);
+        }
     }
 }
