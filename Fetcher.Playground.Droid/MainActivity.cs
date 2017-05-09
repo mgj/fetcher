1d95a1bf9b10c1b4ee336ab87858173d6367f61b
Mikkel Jensen <mgj@artm.dk>
2017-05-09 17:46:03 +0200

    add droid playground

---
+++ Fetcher.Playground.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Fetcher.Playground.Droid
+{
+    [Activity(Label = "Fetcher.Playground.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
