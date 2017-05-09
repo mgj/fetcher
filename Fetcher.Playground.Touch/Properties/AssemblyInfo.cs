efc1aee08445877c44c5da8a2cf69b71deb3d53b
Mikkel Jensen <mgj@artm.dk>
2017-05-09 16:35:59 +0200

    playground.touch

--- Fetcher.Playground.Touch/Properties/AssemblyInfo.cs
+++ Fetcher.Playground.Touch/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fetcher.Playground.Droid")]
+[assembly: AssemblyTitle("Fetcher.Playground.Touch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fetcher.Playground.Droid")]
+[assembly: AssemblyProduct("Fetcher.Playground.Touch")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6f3db4a0-4242-4bb2-94d6-764c09728379")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Fetcher.Playground.Touch/Views/FirstViewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using Cirrious.FluentLayouts.Touch;
+using artm.Fetcher.Core.Services;
+using artm.Fetcher.Touch.Services;
+using artm.Fetcher.Core.Entities;
+using System.Threading.Tasks;
+
+namespace Fetcher.Playground.Touch.Views
+{
+    public class FirstViewController : UIViewController
+    {
+        private IFetcherRepositoryStoragePathService _path;
+        private IFetcherRepositoryService _repository;
+        private IFetcherWebService _web;
+        private IFetcherService _fetcher;
+
+        public FirstViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            View.BackgroundColor = UIColor.White;
+
+            PrepareFetcher();
+
+            var label = PrepareDebugLabel();
+            var button = PrepareDebugButton();
+
+            View.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+            View.AddConstraints(
+                label.AtTopOf(View),
+                label.WithSameWidth(View),
+                label.Height().EqualTo(50)
+                );
+        }
+
+        private async Task PrepareFetcher()
+        {
+            _path = new FetcherRepositoryStoragePathService();
+            _repository = new FetcherRepositoryService(_path);
+            _web = new FetcherWebService();
+
+            _fetcher = new FetcherService(_web, _repository);
+
+            var url = new System.Uri("https://www.google.com");
+            _fetcher.Preload(url, "<html>Hello world!</html>");
+            
+            IUrlCacheInfo response = await _fetcher.Fetch(url);
+        }
+
+        private UIButton PrepareDebugButton()
+        {
+            var result = new UIButton();
+            result.TouchUpInside += Button_TouchUpInside;
+
+            Add(result);
+            return result;
+        }
+
+        private void Button_TouchUpInside(object sender, EventArgs e)
+        {
+            
+        }
+
+        private UILabel PrepareDebugLabel()
+        {
+            var result = new UILabel()
+            {
+                Text = "Hello World"
+            };
+
+            Add(result);
+            return result;
+        }
+    }
+}
